0449eef07f6d39ea765a2acc4141543fda06b1af
Lukman <MuhammedLukman2001@gmai.com>
2023-12-08 18:53:14 +0100

    Time project Added

---
+++ 01-time/app.js
@@ -0,0 +1,54 @@
+const hours = document.querySelector('.hours')
+const min = document.querySelector('.min');
+const seconds = document.querySelector('.seconds');
+const format = document.querySelector('.date-format');
+const greetings = document.querySelector('.greeting');
+
+
+
+function time(){
+    let d = new Date();
+
+    let hour = d.getHours();
+    let mins = d.getMinutes()
+    let second = d.getSeconds();
+    let form = d.toLocaleString('default', {weekday:'long', month: 'long',year: 'numeric', day:'numeric'});
+
+    //checktime
+   
+        mins = checktime(mins)
+        second = checktime(second)
+
+    hours.innerHTML = hour;
+    min.innerHTML = mins;
+    seconds.innerHTML = second;
+    format.innerHTML = form;
+    //greetings
+
+    if(hour < 12){
+        greetings.innerHTML = ' Good Morning...';
+        
+    }
+    else if( hour === 12 || hour > 20){
+        greetings.innerHTML = 'Good Afternoon....'
+    }
+    else{
+        greetings.innerHTML = 'Good Evening....'
+    }
+
+    setTimeout(time, 1000);
+   
+
+
+    function checktime(i){
+        if( i < 10){
+            i = '0' + i;
+        }
+        return i;
+    }
+ 
+   
+}
+
+
+time();
